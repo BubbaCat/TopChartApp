0a5cec233416b0889b72d840b8d07315655502e5
Evgeniy <evgeniy.sapov@gmail.com>
2021-10-03 21:10:59 +0500

    Inital base structure and made first components

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  reactStrictMode: true,
-}
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: { and: [/\.(js|ts)x?$/] },
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
+};
